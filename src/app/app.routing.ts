eee259a1b1dc87977e89b9727c1a5e80235a7fc5
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2021-07-28 22:42:59 +0100

    Adicionando Firebase ao projeto

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,29 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./core/auth/auth.guard";
+import { SigninComponent } from "./core/signin/signin.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HomePageComponent } from "./home/home-page/home-page.component";
 
 const routes: Routes = [
-  { path: "home", component: HomePageComponent },
+  {
+    path: "sigin-in",
+    component: SigninComponent,
+  },
+
+  {
+    path: "home",
+    component: HomePageComponent,
+    canActivate: [AuthGuard],
+  },
+
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+
   {
     path: "**",
     redirectTo: "home",
