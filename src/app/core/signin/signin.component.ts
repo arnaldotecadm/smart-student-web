f222f9391af133eb4dbe3b90585e3251485b1670
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2021-07-30 22:43:34 +0100

    Adicionando Firebase ao projeto

---
+++ src/app/core/signin/signin.component.ts
@@ -0,0 +1,115 @@
+import { Component, NgZone, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import * as firebase from "firebase/app";
+import "firebaseui/dist/firebaseui.css";
+import { AuthService } from "../auth/auth.service";
+import { UserService } from "../user/user.service";
+import firebaseui = require("firebaseui");
+import { TokenService } from "../token/token.service";
+
+@Component({
+  templateUrl: "./signin.component.html",
+  styleUrls: ["./signin.component.css"],
+})
+export class SigninComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(
+    private formBuider: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private userService: UserService,
+    private tokenService: TokenService,
+    private ngZone: NgZone
+  ) {}
+
+  ngOnInit() {
+    if (this.userService.isLogged()) {
+      this.router.navigate(["home"]);
+    }
+
+    this.loginForm = this.formBuider.group({
+      username: ["teste@teste.com", Validators.required],
+      password: ["123456", Validators.required],
+    });
+
+    this.loginForm.valueChanges.subscribe(() => {
+      if (this.loginForm.get("username").value) {
+        this.loginForm.get("username").setErrors(null);
+      }
+
+      if (this.loginForm.get("password").value) {
+        this.loginForm.get("password").setErrors(null);
+      }
+    });
+
+    var config = {
+      callbacks: {
+        signInSuccessWithAuthResult: (authResult) => this.goHome(authResult),
+      },
+      signInOptions: [
+        firebase.default.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.default.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.default.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.default.auth.GithubAuthProvider.PROVIDER_ID,
+      ],
+      signInFlow: "popup",
+    };
+
+    let ui: any;
+
+    if (firebaseui.auth.AuthUI.getInstance()) {
+      ui = firebaseui.auth.AuthUI.getInstance();
+    } else {
+      ui = new firebaseui.auth.AuthUI(firebase.default.auth());
+    }
+
+    ui.start("#firebaseui-auth-container", config);
+  }
+
+  goHome(authResult): boolean {
+    authResult.user.getIdToken().then((token) => {
+      this.tokenService.setToken(token);
+    });
+    this.ngZone.run(() => {
+      this.router.navigate(["home"]);
+    });
+    return false;
+  }
+
+  login() {
+    const userName = this.loginForm.get("username").value;
+    const password = this.loginForm.get("password").value;
+
+    this.authService.authenticate(userName, password);
+
+    /*this.authService.authenticate(userName, password).subscribe(
+      () => this.router.navigate(["/excecoes"]),
+      (err) => {
+        switch (err.status) {
+          case 0:
+            this.msgService.showError(
+              "Ocorreu um erro ao tentar acessar o servidor de Autentições. Tente novamente mais tarde.",
+              "Falha na conexão"
+            );
+            break;
+          case 401:
+            this.msgService.showError(
+              "Usuário ou Senha incorretas.Por favor verifique as informações e tente novamente.",
+              "Acesso não permitido!"
+            );
+            this.loginForm.get("username").setErrors({ userName: true });
+            this.loginForm.get("password").setErrors({ password: true });
+            break;
+          default:
+            this.msgService.showError(
+              "Ocorreu um erro desconhecido, mas não se preocupe;  Já estamos trabalhando para resolver o problema.",
+              "Atenção"
+            );
+            break;
+        }
+      }
+    );*/
+  }
+}
---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { Router } from "@angular/router";
+import * as jwt_decode from "jwt-decode";
+import { BehaviorSubject } from "rxjs";
+import { TokenService } from "../token/token.service";
+import { User } from "./user";
+
+@Injectable({ providedIn: "root" })
+export class UserService {
+  private userSubject = new BehaviorSubject<User>(null);
+
+  constructor(
+    private tokenService: TokenService,
+    private firebaseAuth: AngularFireAuth,
+    private router: Router
+  ) {
+    this.tokenService.hasToken();
+    this.decodeAndNotify();
+  }
+
+  setToken(token: string) {
+    this.tokenService.setToken(token);
+  }
+
+  getUser() {
+    return this.userSubject.asObservable();
+  }
+
+  private decodeAndNotify() {
+    const token = this.tokenService.getToken();
+    if (token) {
+      const user = jwt_decode(token) as User;
+      this.userSubject.next(user);
+    } else {
+      this.logout();
+    }
+  }
+
+  logout() {
+    this.tokenService.removeToken();
+    this.userSubject.next(null);
+    this.firebaseAuth.signOut();
+
+    this.router.navigate(["/sigin-in"]);
+  }
+
+  public isLogged() {
+    return this.tokenService.hasToken();
+  }
+}
