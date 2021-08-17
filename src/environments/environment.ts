3f40fcd40e37e29b2f28b6565e4c1789fa8a1d79
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2021-08-17 23:37:34 +0100

    Changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,14 @@
 export const environment = {
   production: false,
   ApiUrl: "http://localhost:8080",
-  firebaseConfig: {},
+  firebaseConfig: {
+    apiKey: "AIzaSyCBXhTc8pGIXqKK49cS-Clk2EIWA3cT8-0",
+    authDomain: "smart-student-d3812.firebaseapp.com",
+    databaseURL: "https://smart-student-d3812-default-rtdb.firebaseio.com",
+    projectId: "smart-student-d3812",
+    storageBucket: "smart-student-d3812.appspot.com",
+    messagingSenderId: "615510489873",
+    appId: "1:615510489873:web:bf780e60888351ad732ab0",
+    measurementId: "G-3RZHE4B4Y7",
+  },
 };
