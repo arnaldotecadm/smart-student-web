c3b40b4952072ba1a65e8531f9676d4da263cd86
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2021-08-17 23:38:21 +0100

    Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,5 @@
 export const environment = {
   production: true,
   ApiUrl: "http://www.arcasoftwares.com.br:8080",
-  firebaseConfig: {
-    apiKey: "AIzaSyCBXhTc8pGIXqKK49cS-Clk2EIWA3cT8-0",
-    authDomain: "smart-student-d3812.firebaseapp.com",
-    databaseURL: "https://smart-student-d3812-default-rtdb.firebaseio.com",
-    projectId: "smart-student-d3812",
-    storageBucket: "smart-student-d3812.appspot.com",
-    messagingSenderId: "615510489873",
-    appId: "1:615510489873:web:bf780e60888351ad732ab0",
-    measurementId: "G-3RZHE4B4Y7",
-  },
+  firebaseConfig: {},
 };
