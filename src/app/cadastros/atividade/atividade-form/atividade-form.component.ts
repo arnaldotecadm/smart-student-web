bb8f522b2d6217a45a90823f707abd88a4b688a9
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2021-08-20 09:13:48 +0100

    Adicao de componentes para o crud de atividades

--- src/app/cadastros/atividade/atividade-form/atividade-form.component.ts
+++ src/app/cadastros/atividade/atividade-form/atividade-form.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { FormBuilder, FormGroup, NgForm, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormCanDeactivate } from "app/guards/form-can-deactivate.component";
+import { Observable } from "rxjs";
+import { AtividadeService } from "../atividade.service";
 
 @Component({
-  selector: 'app-atividade-form',
-  templateUrl: './atividade-form.component.html',
-  styleUrls: ['./atividade-form.component.css']
+  selector: "app-atividade-form",
+  templateUrl: "./atividade-form.component.html",
+  styleUrls: ["./atividade-form.component.css"],
 })
-export class AtividadeFormComponent implements OnInit {
+export class AtividadeFormComponent
+  extends FormCanDeactivate
+  implements OnInit
+{
+  @ViewChild("formAtividade") ngForm: NgForm;
 
-  constructor() { }
+  identifier: string;
+  obj$: Observable<any>;
+  formulario: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private service: AtividadeService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    super();
   }
 
+  ngOnInit(): void {
+    this.construirFormulario();
+    this.carregarDados();
+  }
+
+  get form(): NgForm {
+    return this.ngForm;
+  }
+
+  carregarDados() {
+    this.identifier = this.route.snapshot.paramMap.get("identificador");
+    this.service.getById(this.identifier).subscribe((data) => {
+      if (data) {
+        this.formulario.patchValue(data);
+      }
+    });
+  }
+
+  voltar() {
+    this.router.navigate(["atividades"]);
+  }
+
+  update() {
+    Object.keys(this.formulario.controls).forEach((field) => {
+      const control = this.formulario.get(field);
+      control.markAsTouched({ onlySelf: true });
+    });
+
+    if (this.formulario.invalid) {
+      return;
+    }
+    this.service
+      .salvarRegistro(this.formulario.getRawValue())
+      .subscribe((resposta: any) => {
+        this.ngForm.resetForm();
+        this.voltar();
+      });
+  }
+
+  private construirFormulario() {
+    this.formulario = this.formBuilder.group({
+      documentId: [],
+      id: [{ value: "", disabled: true }],
+      nome: ["", Validators.required],
+      descricao: [""],
+    });
+  }
 }
--- src/app/cadastros/atividade/atividade-form/atividade-form.module.ts
+++ src/app/cadastros/atividade/atividade-form/atividade-form.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AtividadeFormComponent } from "./atividade-form.component";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AtividadeFormComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatExpansionModule,
+    ReactiveFormsModule,
+  ],
   exports: [AtividadeFormComponent],
 })
 export class AtividadeFormModule {}
