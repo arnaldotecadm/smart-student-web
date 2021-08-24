141d431c5fb0f5b78cbf69cabc5e760a0fb7688e
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2021-08-24 15:50:15 +0100

    Persistir cadastros auxiliares

--- src/app/cadastros/atividade/atividade-form/atividade-form.component.ts
+++ src/app/cadastros/atividade/atividade-form/atividade-form.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup, NgForm, Validators } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
+import { MateriaService } from "app/cadastros/materia/materia.service";
+import { ProfessorService } from "app/cadastros/professor/professor.service";
+import { TipoAtividadeService } from "app/cadastros/tipo-atividade/tipo-atividade.service";
+import { TurmaService } from "app/cadastros/turma/turma.service";
 import { FormCanDeactivate } from "app/guards/form-can-deactivate.component";
 import { Observable } from "rxjs";
 import { AtividadeService } from "../atividade.service";
@@ -20,11 +24,20 @@ export class AtividadeFormComponent
   obj$: Observable<any>;
   formulario: FormGroup;
 
+  todasTurmas$: Observable<any>;
+  todasMaterias$: Observable<any>;
+  todosProfessores$: Observable<any>;
+  todosTipoAtividade$: Observable<any>;
+
   constructor(
     private service: AtividadeService,
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private turmaService: TurmaService,
+    private materiaService: MateriaService,
+    private professorService: ProfessorService,
+    private tipoAtividadeService: TipoAtividadeService
   ) {
     super();
   }
@@ -40,6 +53,12 @@ export class AtividadeFormComponent
 
   carregarDados() {
     this.identifier = this.route.snapshot.paramMap.get("identificador");
+
+    this.todasTurmas$ = this.turmaService.getAll();
+    this.todasMaterias$ = this.materiaService.getAll();
+    this.todosProfessores$ = this.professorService.getAll();
+    this.todosTipoAtividade$ = this.tipoAtividadeService.getAll();
+
     this.service.getById(this.identifier).subscribe((data) => {
       if (data) {
         this.formulario.patchValue(data);
@@ -64,7 +83,6 @@ export class AtividadeFormComponent
       .salvarRegistro(this.formulario.getRawValue())
       .subscribe((resposta: any) => {
         this.ngForm.resetForm();
-        this.voltar();
       });
   }
 
@@ -74,6 +92,10 @@ export class AtividadeFormComponent
       id: [{ value: "", disabled: true }],
       nome: ["", Validators.required],
       descricao: [""],
+      turma: [],
+      materia: [],
+      professor: [],
+      tipoAtividade: [],
     });
   }
 }
