9324ad934df5bb93de2aea2e028f4a98ee397d65
Arnaldo C. Bezerra <arnaldo.bezerra@waitrose.co.uk>
2021-11-24 23:35:20 +0000

    Alterando arquivos de conf (TEST CREDDENTIALS)

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+
+export const environment = {
+  production: false,
+  ApiUrl: "http://localhost:8080",
+  firebaseConfig: {
+    apiKey: "AIzaSyCBXhTc8pGIXqKK49cS-Clk2EIWA3cT8-0",
+    authDomain: "smart-student-d3812.firebaseapp.com",
+    databaseURL: "https://smart-student-d3812-default-rtdb.firebaseio.com",
+    projectId: "smart-student-d3812",
+    storageBucket: "smart-student-d3812.appspot.com",
+    messagingSenderId: "615510489873",
+    appId: "1:615510489873:web:bf780e60888351ad732ab0",
+    measurementId: "G-3RZHE4B4Y7",
+  },
+};
